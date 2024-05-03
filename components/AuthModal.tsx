d533b9e48b40f90ed6638c78c3ec5268bf9e3f05
sajithkavishan- <sajithkavishan1111@gmail.com>
2024-05-03 05:37:21 +0530

    update Modals

---
+++ components/AuthModal.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+import Modal from "./Modal";
+
+const AuthModal = () => {
+  return (
+    <Modal
+      title="Welcome back"
+      description="Login to your account"
+      onChange={() => {}}
+    >
+      Auth Modal Children!
+    </Modal>
+  );
+};
+
+export default AuthModal;
