2a239c5002f50138ca5a60b203f9e79980dc05a7
sajithkavishan- <sajithkavishan1111@gmail.com>
2024-05-08 04:30:39 +0530

    con

---
+++ components/UploadModal.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import useUploadModal from "@/hooks/useUploadModal";
+
+import Modal from "./Modal";
+
+const UploadModal = () => {
+  const uploadModal = useUploadModal();
+
+  const onChange = (open: boolean) => {
+    if (!open) {
+      //reset the form
+      uploadModal.onClose();
+    }
+  };
+
+  return (
+    <Modal
+      title="Add a song"
+      description="Upload an mp3 file"
+      isOpen={uploadModal.isOpen}
+      onChange={onChange}
+    >
+      Form
+    </Modal>
+  );
+};
+
+export default UploadModal;
